126454638f083bfe20777bf711931003333bb641
Dusan Jankovic <jankovic.dushan@gmail.com>
2018-01-12 21:27:20 +0100

    Staged all files

--- LoginForm/Form1.cs
+++ LoginForm/Form1.cs
@@ -9,7 +9,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Data.SqlClient;
 
-namespace WindowsFormsApp1
+namespace LoginForm
 {
     public partial class Form1 : Form
     {
--- LoginForm/Main.Designer.cs
+++ LoginForm/Main.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace WindowsFormsApp1
+﻿namespace LoginForm
 {
     partial class Main
     {
